cbaff3eae06f086d31c268c2f8b859d2b94928d9
meet2701 <parekhmeet997@gmail.com>
2025-03-12 10:00:00 +0530

    Class Activity 2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ISS-Class-Activity-2
+
+## Changes that i made in the code :
+
+Line 1,10,12,13 - Added ':' at the end of the line
+Line 10 - Changed the name of function print_narcis_numbers to print_narc_numbers
+Line 10 - Added , after start  
+Line 13 - Changed if_narcissistic to is_narc
+Line 3,4 - Changed == to = in num_str and num_digits
+Line 6 - Changed *** to **
+
+## LLM usage :
+
+To confirm definition of Narc numbers
---
+++ eagle_test.py
@@ -0,0 +1,23 @@
+def is_narc(n):
+    """Check if a number is narc."""
+    num_str = str(n)
+    num_digits = len(num_str)
+    
+    sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
+    
+    return sum_of_powers == n
+
+def print_narc_numbers(start, end):
+    """Print all narc numbers in a given range."""
+    for num in range(start, end + 1):
+        if is_narc(num):
+            print(num)
+
+print_narc_numbers(1000, 5000)
+
+#Added : first
+#Changed the name of function narcis to narc
+#Added , after start
+#Changed if_narcissistic to is_narc"
+#Changed == to = in num_str and num_digits
+#Changed *** to **
